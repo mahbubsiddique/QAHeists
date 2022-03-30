e80bc1598b888eda1729ad600734043cca82fa29
mahbubsiddique <mahbubsiddique@localhost>
2022-03-29 21:04:08 -0400

    updated the spacing on testrunner

--- src/test/java/com/careerhack/runners/TestRunner.java
+++ src/test/java/com/careerhack/runners/TestRunner.java
@@ -4,8 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
+		
+		//updated the spacing
+		
 		features = "Features",
 		glue = "com.careerhack.steps"
+		
 		
 		)
 
